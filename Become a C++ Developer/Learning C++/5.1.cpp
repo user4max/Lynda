14570329a770f6c71915c0ceb7339de51508cad0
Carp-bezverhnii MAXIM <cmaxim@e1p35.chisinau.42.fr>
2018-05-30 23:10:19 +0300

    30.05.2018

---
+++ Become a C++ Developer/Learning C++/5.1.cpp
@@ -0,0 +1,33 @@
+//Arrays
+#include <iostream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	//array declarations
+	string seasons[4];
+	int number[10];
+	for(int i = 0; i<10; i++)
+	{
+		number[i] = rand();
+	}
+
+	//declare and initialize the array
+	string weekdays[] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+
+	//change the value of an array
+	seasons[0] = "summer";
+	seasons[1] = "fall";
+	seasons[2] = "winter";
+	seasons[3] = "spring";
+
+	cout << seasons << endl;
+	cout << seasons[0] << endl;
+	cout << seasons[1] << endl;
+	cout << seasons[2] << endl;
+	cout << seasons[3] << endl;
+
+	return 0;
+}
