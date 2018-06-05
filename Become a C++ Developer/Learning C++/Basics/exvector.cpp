815df03b24b3fe5fd52e222335112741bda6ab8d
maxim.carp-bezverhnii <maxim@smartsoftdev.eu>
2018-06-05 17:38:10 +0300

    05.06.2018

---
+++ Become a C++ Developer/Learning C++/Basics/exvector.cpp
@@ -0,0 +1,23 @@
+//STL vector
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	vector<int> myVector;
+	myVector.push_back(2);
+	myVector.push_back(44);
+	myVector.push_back(77);
+
+
+	/*
+	cout << "Number of elemtens in vector : "<< myVector.size() << endl;
+
+	for (int i = 0; i < myVector.size(); i++)
+	{
+		cout << myVector[i] << endl;
+	}
+	*/
+	return 0;
+}
---
+++ Become a C++ Developer/Learning C++/Data Structures/MyChallenge.cpp
@@ -0,0 +1,7 @@
+//Print out a lunch menu
+//It will have 4 items along with 4 prices
+//ask the user to choose an item
+//after they select which item they want, ask for
+//quantity (how many they want), print toal cost of their order
+//create 2 parallel arrays.1 contain the lucnh items, such as
+//hotdog, hamburger, french fries and salad.Use a fifth option to allow the //user to exit.Have a corresponding parallel array that has the price of ach //item, the price of a hotdog, the price of a hamburger, the price of french //fries, the price of a salad.docker pull smartmaxeu/yocto-project
